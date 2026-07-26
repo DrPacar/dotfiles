bb5264eb22a25b0a4a6de56ae8f5bf17e4bd8400
DrPacar <lukapacar06@gmail.com>
2026-07-26 16:03:18 +0200

    add tailscale

---
+++ modules/system/network/tailscale.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ... }: {
+  services.tailscale.enable = true;
+
+  networking.firewall = {
+    trustedInterfaces = [ config.services.tailscale.interfaceName ];
+    allowedUDPPorts = [ config.services.tailscale.port ];
+  };
+}
