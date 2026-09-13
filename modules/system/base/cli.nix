6f6d6a7ecbfe55a14ead27d15b9a1f11f35cfb1f
DrPacar <lukapacar06@gmail.com>
2026-09-13 12:15:56 +0200

    expand base cli commands

--- modules/system/base/cli.nix
+++ modules/system/base/cli.nix
@@ -1,11 +1,27 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
+    # file & disk
+    fd
+    ripgrep
+    bat
+    eza
+    yazi
+    duf
+    # network
+    nmap
+    dig
+    traceroute
     # archives
     zip
     unzip
-    ouch
     # downloading
     wget
     curl
+    # general
+    git
+    jq
+    gcc
+    # nix
+    direnv
   ];
 }
